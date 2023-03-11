3042bfea88792d3fc94c514d07b09615fda4e262
Estela Martínez Lozano <emarloz@upv.edu.es>
2023-03-11 12:04:04 +0100

    registro UI

---
+++ Activity1.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace pruebasEF
+{
+    [Activity(Label = "Activity1")]
+    public class Activity1 : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            // Create your application here
+        }
+    }
+}
