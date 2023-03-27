8b9ffdd83f58aa0961d13714a2b1f3701a48e0d9
Estela Martínez Lozano <emarloz@upv.edu.es>
2023-03-27 12:45:31 +0200

    usuarioRepositorio

---
+++ Persistencia/Repository/UsuarioRepositorio.cs
@@ -0,0 +1,29 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using preguntaods.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace preguntaods.Persistencia.Repository
+{
+    internal class UsuarioRepositorio : Repository<Usuario>
+    {
+        public async Task<Usuario> GetUserByName(String id)
+        {
+            var a = SingletonConexion.getInstance();
+            var response = await a.cliente
+                .From<Usuario>()
+                .Where(x => x.nombre == id)
+                .Single();
+
+            return response;
+        }
+    }
+}
