11741a4982d11d4fa71764ed36c5a19e224ac122
Andreu Raga Català <ARAGCAT@upv.edu.es>
2023-04-09 16:30:38 +0200

    Arreglos a la clase RepositorioConfiguracion y se ha añadido la clase RepositorioUsuario

--- Persistencia/Repository/impl/RepositorioConfiguracion.cs
+++ Persistencia/Repository/impl/RepositorioConfiguracion.cs
@@ -5,12 +5,12 @@ namespace preguntaods.Persistencia.Repository
 {
     internal class RepositorioConfiguracion : Repository<Usuario>
     {
-        public async Task<Usuario> GetUserByName(int id)
+        public async Task<Usuario> GetUserByName(string nombre)
         {
             var a = SingletonConexion.GetInstance();
             var response = await a.cliente
                 .From<Usuario>()
-                .Where(x => x.Id == id)
+                .Where(x => x.Nombre == nombre)
                 .Single();
 
             return response;
---
+++ Persistencia/Repository/impl/RepositorioUsuario.cs
@@ -0,0 +1,18 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using preguntaods.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace preguntaods.Persistencia.Repository.impl
+{
+    public class RepositorioUsuario : Repository<Usuario>
+    {
+    }
+}
