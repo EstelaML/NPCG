2a22a16ebf909c35a4c5d5e90a660eda2418bc14
Pablo <PRODGOM@upv.edu.es>
2023-04-17 22:05:11 +0200

    clean code y arreglo de código

--- Persistencia/Repository/impl/RepositorioPregunta.cs
+++ Persistencia/Repository/impl/RepositorioPregunta.cs
@@ -17,6 +17,7 @@ namespace preguntaods.Persistencia.Repository
 
         public async Task<List<Pregunta>> GetByDificultad(int dificultad)
         {
+
             var id = (conexion.usuario.Id);
             var user = await conexion.cliente.From<Usuario>().Where(x => x.Uuid == id).Single();
             var response = await conexion.cliente.From<Pregunta>().Where(x => x.Dificultad == dificultad).Get();
@@ -27,38 +28,5 @@ namespace preguntaods.Persistencia.Repository
 
             return preguntas ?? new List<Pregunta>();
         }
-
-        /*
-        public async Task<List<Pregunta>> GetByDificultad(int dificultad, List<Reto> retos)
-        {
-            var a = conexion.usuario.Id;
-            // cojo todas las Preguntas y las paso a una lista
-            var preguntasTodos = await conexion.cliente
-                .From<Pregunta>()
-                .Where(x => x.Dificultad == dificultad)
-                .Get();
-
-            List<Pregunta> preguntasTodasLista = preguntasTodos.Models.AsEnumerable().ToList();
-
-            // cojo del usuario Loggeado las preguntas que haya acertado y estén en la BD
-            var retosRealizados = await conexion.cliente.From<Usuario>().Where(x => x.Uuid == a).Single();
-            int[] retosID = retosRealizados.PreguntasRealizadas;
-            List<Pregunta> preguntasPosibles = preguntasTodasLista;
-            if (retosID != null)
-            {
-                List<int> preguntasRealizadas = retosID.ToList();
-                // elimino de la lista de preguntas ya realizadas
-                preguntasPosibles = preguntasTodasLista.Where(x => x.Id.HasValue && !preguntasRealizadas.Contains((int)x.Id)).Select(x => x).ToList();
-            }
-            if (retos != null)
-            {
-                // !retos
-                List<int> preguntasRetoRealizada = retos.Cast<RetoPre>().Where(x => x.GetPregunta()?.Id.HasValue == true).Select(x => x.GetPregunta().Id.Value).ToList();
-                preguntasPosibles = preguntasPosibles.Where(x => !preguntasRetoRealizada.Contains((int)x.Id)).ToList();
-            }
-            //return response.Models.AsEnumerable();
-            return preguntasPosibles;
-        }
-        */
     }
 }
