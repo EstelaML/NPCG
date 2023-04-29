cee21446ef4cc8dae2efced4d02af74d158a1177
Pablo <pablinelchulin@gmail.com>
2023-04-29 13:37:24 +0200

    limpieza

--- Persistencia/Repository/impl/RepositorioUsuario.cs
+++ Persistencia/Repository/impl/RepositorioUsuario.cs
@@ -83,40 +83,5 @@ namespace preguntaods.Persistencia.Repository.impl
 
             return null;
         }
-
-        public async Task<int[]> GetRetoAcertado(string a, Reto reto)
-        {
-            return null;
-            /*var usuario = await conexion.Cliente.From<Usuario>().Where(x => x.Uuid == a).Single();
-            int id = (int)usuario.Id;
-            if (reto is RetoPre) {
-                // solicitas los RetosRealizados pero solo la columna de preguntas donde preguntas != null
-                var retosPreguntas = await conexion.Cliente
-                    .From<RetosRealizados>()
-                    .Where(x => x.Usuario == id)
-                    .Where(c => c.PreguntasRealizadas != null).Get();
-                var listaPreguntasRealizadas = retosPreguntas.Models;
-                var respuesta = new int[listaPreguntasRealizadas.Count];
-                for (int i = 0; i < listaPreguntasRealizadas.Count; i++)
-                {
-                    respuesta[i] = (int)listaPreguntasRealizadas[i].Pregunta;
-                }
-                return respuesta;
-            } else if (reto is RetoAhorcado)
-            {
-                var retosPreguntas = await conexion.Cliente
-                     .From<RetosRealizados>()
-                     .Where(x => x.Usuario == id)
-                     .Where(c => c.Ahorcado != null).Get();
-                var listaPreguntasRealizadas = retosPreguntas.Models;
-                var respuesta = new int[listaPreguntasRealizadas.Count];
-                for (int i = 0; i < listaPreguntasRealizadas.Count; i++)
-                {
-                    respuesta[i] = (int)listaPreguntasRealizadas[i].Pregunta;
-                }
-                return respuesta;
-            } //... diferentes retos
-            return null;*/
-        }
     }
 }
