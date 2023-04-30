f4fd77d5a23e3f98afc4c2ffe9eb583d310b8281
Pablo <pablinelchulin@gmail.com>
2023-04-30 11:57:53 +0200

    merge

--- BusinessLogic/Services/Facade.cs
+++ BusinessLogic/Services/Facade.cs
@@ -103,10 +103,13 @@ namespace preguntaods.BusinessLogic.Services
 
         #endregion Usuario
 
-        public async Task<List<Usuario>> GetPuntuaciones()
+        public async Task<List<int>> GetPuntuaciones()
         {
             var respuesta = await repositorioUser.GetAll();
-            return (List<Usuario>)respuesta.ToList().OrderBy(usuario => usuario.Puntos).Take(20);
+            var listaUsuarios = (List<Usuario>)respuesta.ToList().OrderBy(usuario => usuario.Puntos).Take(20);
+            var puntuaciones = listaUsuarios.Select(usuario => usuario.Puntos).ToList();
+
+            return puntuaciones;
         }
     }
 }
