7418aa2446cb9b2a91299e7883259df41fdea035
Miguel Prosper Asensi <MPROASE@upv.edu.es>
2023-05-01 17:27:09 +0200

    añadida persintencia y logica clase Estadisticas

---
+++ BusinessLogic/Entities/Estadisticas.cs
@@ -0,0 +1,19 @@
+﻿
+namespace preguntaods.Entities
+{
+    public partial class Estadisticas
+    {
+        public Estadisticas() 
+        { }
+
+        public Estadisticas(int user, int puntuacion, int[] aciertos, int[] fallos)
+        { 
+        
+            Usuario = user;
+            Puntuacion = puntuacion;
+            Aciertos = aciertos;
+            Fallos = fallos;
+        
+        }
+    }
+}
---
+++ Persistencia/Entities/Estadisticas.cs
@@ -0,0 +1,26 @@
+﻿using Postgrest.Attributes;
+using Postgrest.Models;
+
+
+namespace preguntaods.Entities
+{
+
+    [Table("Estadisticas")]
+    public partial class Estadisticas : BaseModel, IEntity
+    {
+        [PrimaryKey("id")]
+        public int? Id { get; set; }
+
+        [Column("Usuario")]
+        public int Usuario { get; set; }
+
+        [Column("Puntuacion")]
+        public int Puntuacion { get; set;}
+
+        [Column("Aciertos")]
+        public int[] Aciertos { get; set; }
+
+        [Column("Fallos")]
+        public int[] Fallos { get; set; }
+    }
+}
