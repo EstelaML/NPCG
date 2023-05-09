b503fb3a2ff51ae882bee16fdc72fb91344125ce
Estela Martínez Lozano <emarloz@upv.edu.es>
2023-05-09 21:45:46 +0200

    inicio de sesión contraseña visible

--- Presentacion/ViewModels/InicioSesionViewModel.cs
+++ Presentacion/ViewModels/InicioSesionViewModel.cs
@@ -2,6 +2,7 @@
 using Android.App;
 using Android.Content;
 using Android.OS;
+using Android.Text;
 using Android.Widget;
 using AndroidX.AppCompat.App;
 using preguntaods.BusinessLogic.EstrategiaSonido;
@@ -20,6 +21,8 @@ namespace preguntaods.Presentacion.ViewModels
         private TextView registrar;
         private Facade fachada;
         private Sonido sonido;
+        private bool vistaContraseña = false;
+        private ImageView ojo;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -36,6 +39,7 @@ namespace preguntaods.Presentacion.ViewModels
             // Create your application here
             correo = FindViewById<EditText>(Resource.Id.correo);
             password = FindViewById<EditText>(Resource.Id.contraseña);
+            password.InputType = InputTypes.TextVariationPassword | InputTypes.ClassText;
 
             iniciarSesion = FindViewById<Button>(Resource.Id.inicioSesion);
             if (iniciarSesion != null) iniciarSesion.Click += IniciarSesion_Click;
@@ -44,6 +48,26 @@ namespace preguntaods.Presentacion.ViewModels
 
             registrar = FindViewById<TextView>(Resource.Id.registrar);
             if (registrar != null) registrar.Click += NavigateRegistro;
+
+            ojo = FindViewById<ImageView>(Resource.Id.ojoContraseña);
+            ojo.Click += Ojo_Click;
+        }
+
+        private void Ojo_Click(object sender, EventArgs e)
+        {
+            vistaContraseña = !vistaContraseña;
+            var type = password.InputType;
+            if (vistaContraseña)
+            {
+                // se muestra
+                password.InputType = InputTypes.TextVariationVisiblePassword;
+                ojo.SetImageResource(Resource.Drawable.ojo_cerrado);
+            }
+            else {
+                // no se muestra
+                password.InputType = InputTypes.TextVariationPassword | InputTypes.ClassText;
+                ojo.SetImageResource(Resource.Drawable.ojo_abierto);
+            }
         }
 
         private void NavigateRegistro(object sender, EventArgs e)
