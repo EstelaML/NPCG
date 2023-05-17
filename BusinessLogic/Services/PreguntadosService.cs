91a086558ab42b6b0e3934c20d5c5eb56ed3572b
Pablo <PRODGOM@upv.edu.es>
2023-05-17 15:58:07 +0200

    fix

--- BusinessLogic/Services/PreguntadosService.cs
+++ BusinessLogic/Services/PreguntadosService.cs
@@ -263,11 +263,11 @@ namespace preguntaods.BusinessLogic.Services
             {
                 // añado a la BD ese reto
                 case IReto.TypePregunta:
-                    await repositorioPregunta.AñadirPreguntaFallada(reto);
+                    await repositorioPregunta.AñadirPreguntaFallada(((RetoPre)reto).GetPregunta());
                     break;
 
                 case IReto.TypeAhorcado:
-                    await repositorioAhorcado.AñadirAhorcadoFallado(reto);
+                    await repositorioAhorcado.AñadirAhorcadoFallado(((RetoAhorcado)reto).GetAhorcado());
                     break;
             }
         }
