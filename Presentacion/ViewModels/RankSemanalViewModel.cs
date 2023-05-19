fb9c109fd0151ddcdaa7aeaeb30308e2d8cb569a
Andreu Raga Català <ARAGCAT@upv.edu.es>
2023-05-19 13:21:47 +0200

    Creadas las clases RankDiarioViewModel y RankSemanalViewModel

---
+++ Presentacion/ViewModels/RankSemanalViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Content.Res;
+using Android.OS;
+using Android.Views;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using preguntaods.BusinessLogic.EstrategiaSonido;
+using preguntaods.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using preguntaods.BusinessLogic.Fachada;
+
+namespace preguntaods.Presentacion.ViewModels
+{
+    [Activity(Label = "", Theme = "@style/HiddenTitleTheme")]
+    public class RankSemanalViewModel : RankingViewModel
+    {
+        protected override async void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+        }
+    }
+}
