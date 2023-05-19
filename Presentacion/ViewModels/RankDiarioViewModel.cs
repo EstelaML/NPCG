7338fbf13563c54094f3a4e0b61266fadbba880c
Andreu Raga Català <ARAGCAT@upv.edu.es>
2023-05-19 23:57:18 +0200

    Agregada funcionalidad a RankDiarioViewModel y RankSemanalViewModel

--- Presentacion/ViewModels/RankDiarioViewModel.cs
+++ Presentacion/ViewModels/RankDiarioViewModel.cs
@@ -17,9 +17,26 @@ namespace preguntaods.Presentacion.ViewModels
     [Activity(Label = "", Theme = "@style/HiddenTitleTheme")]
     public class RankDiarioViewModel : RankingViewModel
     {
+        private TextView tipoRanking;
+        private Button botonIzq;
         protected override async void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+            tipoRanking = FindViewById<TextView>(Resource.Id.tipoRanking);
+            tipoRanking.Text = "Ranking diario";
+
+            botonIzq = FindViewById<Button>(Resource.Id.botonIzq);
+            botonIzq.Text = "General";
+            if (botonIzq != null) { botonIzq.Click += BotonIzq; }
+        }
+
+        private void BotonIzq(object sender, EventArgs e)
+        {
+            sonido.SetEstrategia(new EstrategiaSonidoClick(), this);
+            sonido.EjecutarSonido();
+
+            var i = new Intent(this, typeof(RankingViewModel));
+            StartActivity(i);
         }
     }
 }
--- Presentacion/ViewModels/RankSemanalViewModel.cs
+++ Presentacion/ViewModels/RankSemanalViewModel.cs
@@ -17,9 +17,26 @@ namespace preguntaods.Presentacion.ViewModels
     [Activity(Label = "", Theme = "@style/HiddenTitleTheme")]
     public class RankSemanalViewModel : RankingViewModel
     {
+        private TextView tipoRanking;
+        private Button botonDer;
         protected override async void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+            tipoRanking = FindViewById<TextView>(Resource.Id.tipoRanking);
+            tipoRanking.Text = "Ranking semanal";
+
+            botonDer = FindViewById<Button>(Resource.Id.botonDer);
+            botonDer.Text = "General";
+            if (botonDer != null) { botonDer.Click += BotonDer; }
+        }
+
+        private void BotonDer(object sender, EventArgs e)
+        {
+            sonido.SetEstrategia(new EstrategiaSonidoClick(), this);
+            sonido.EjecutarSonido();
+
+            var i = new Intent(this, typeof(RankingViewModel));
+            StartActivity(i);
         }
     }
 }
