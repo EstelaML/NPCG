a8b07532ea08416b94152a2b66c51fccda0c21ac
Miguel Prosper Asensi <MPROASE@upv.edu.es>
2023-05-23 16:34:09 +0200

    añadido pop up de carga en perfil y selecionReto

--- Presentacion/ViewModels/SeleccionRetoViewModel.cs
+++ Presentacion/ViewModels/SeleccionRetoViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Android.App;
+﻿using Acr.UserDialogs;
+using Android.App;
 using Android.Content;
 using Android.Graphics;
 using Android.OS;
@@ -9,6 +10,7 @@ using preguntaods.BusinessLogic.EstrategiaSonido;
 using preguntaods.BusinessLogic.Fachada;
 using preguntaods.Entities;
 using System;
+using System.Threading.Tasks;
 
 namespace preguntaods.Presentacion.ViewModels
 {
@@ -38,6 +40,8 @@ namespace preguntaods.Presentacion.ViewModels
 
             usuario = await fachada.GetUsuarioLogged();
 
+            UserDialogs.Init(this);
+
             partida = FindViewById<Button>(Resource.Id.partidaB);
             if (partida != null) partida.Click += Partida_Click;
 
@@ -54,14 +58,20 @@ namespace preguntaods.Presentacion.ViewModels
             atras = FindViewById<ImageButton>(Resource.Id.button1);
             if (atras != null) atras.Click += Atras;
 
-            InitBoton();
+            UserDialogs.Instance.ShowLoading("Cargando...");
+
+            await InitBoton();
+
+            UserDialogs.Instance.HideLoading();
         }
 
-        public void InitBoton()
+        public async Task InitBoton()
         {
             level = usuario.Nivel;
 
             var color = new Color(150, 220, 240);
+
+            await Task.Delay(500);
 
             if (level == 1)
             {
@@ -76,6 +86,8 @@ namespace preguntaods.Presentacion.ViewModels
                 fiesta.Enabled = true;
                 fiesta.SetBackgroundColor(color);
             }
+
+            
         }
 
         public override bool OnCreateOptionsMenu(IMenu menu)
